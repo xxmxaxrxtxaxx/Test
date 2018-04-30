6f5d38d3af89bf2b81c2a3230f80553a59303dc2
xxmxaxrxtxaxx <xxmxaxrxtxaxx@gmail.com>
2018-04-30 18:08:02 +0200

    dodane okno edycji

--- projekt.js
+++ projekt.js
@@ -1,5 +1,8 @@
 const express = require('express');
 var mysql = require('sync-mysql');
+var bodyParser = require('body-parser')
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false})
 
 const app = express();
 
@@ -14,8 +17,8 @@ var szablon = function (body) {
     var text = ''
     text += '<html><head>'
     text += '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">'
-    text+='</head><body>';
-    text +='<div class="navbar bg-dark"><a class="navbar-brand" href="#">Projekt Testy</a></div>'
+    text += '</head><body>';
+    text += '<div class="navbar bg-dark"><a class="navbar-brand" href="#">Projekt Testy</a></div>'
     text += '<div class="row">'
     text += body;
     text += '</div>'
@@ -31,14 +34,33 @@ var stronaGlowna = function () {
     var html = '';
     html += '<table class="table table-striped"><tr><th>Nazwa</th><th>Akcje</th></tr>'
     for (var i = 0; i < testy.length; i++) {
-        html += '<tr><td>' + testy[i].nazwa + '</td><td><a href="test/' + testy[i].id + '">Wyniki</a></td></tr>';
+        html += '<tr><td>' + testy[i].nazwa + '</td>'
+        html += '<td><a href="test/' + testy[i].id + '">Wyniki</a><a href="edytuj/' + testy[i].id + '">Edytuj</a></td>'
+        html += '</tr>';
     }
     html += '</table>'
+    return html;
+}
+
+var edytujTest = function (id) {
+    var test = con.query("select nazwa, id from testy where id=" + id)[0];
+
+    var html = '<form class="form form-horizontal" method="POST" action="/zapisz">';
+    html += '<input class="form-control" type="hidden" name="id" value="' + test.id + '"/>';
+    html += '<input class="form-control" type="text" name="nazwa" value="' + test.nazwa + '"/>';
+    html += '<input class="button button-primary" type="submit"/>';
+    html += '</form>';
     return html;
 }
 
 
 app.get('/', (req, res) => res.send(szablon(stronaGlowna())));
 app.get('/test/:id', (req, res) => res.send('tu bedą wyniki testu nr' + req.params.id));
+app.get('/edytuj/:id', (req, res) => res.send(szablon(edytujTest(req.params.id))));
+app.post('/zapisz', urlencodedParser,(req, res) => {
+
+    con.query('update testy set nazwa = \'' + req.body.nazwa + '\' where id = ' + req.body.id);
+    res.redirect('/');
+})
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
