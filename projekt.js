f1c41e94c5adaca1e98a5f700b448d9b71651ebc
xxmxaxrxtxaxx <xxmxaxrxtxaxx@gmail.com>
2018-05-02 11:26:58 +0200

    zrobione dodawanie testow i pytan

--- projekt.js
+++ projekt.js
@@ -2,6 +2,7 @@ const express = require('express');
 var mysql = require('sync-mysql');
 var bodyParser = require('body-parser')
 var ejs = require('ejs');
+var format = require('string-format');
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false})
 
@@ -23,20 +24,35 @@ var stronaGlowna = function (req, res) {
     ejs.renderFile("Views\\home.ejs",model,function(err,str){if(err) throw err; res.send(str);})
 }
 
-var edytujTest = function (req, res) {
-    var test = con.query("select nazwa, id from testy where id=" + req.params.id)[0];
-
-    ejs.renderFile("Views\\edytuj.ejs",test,function(err,str){if(err) throw err; res.send(str);})
+var dodajTest = function (req, res) {
+    ejs.renderFile("Views\\edytuj.ejs",{},function(err,str){if(err) throw err; res.send(str);})
 }
 
 var zapiszTest = function(req, res){
-    con.query('update testy set nazwa = \'' + req.body.nazwa + '\' where id = ' + req.body.id);
-    res.redirect('/');
+    var id= con.query(format("insert into testy (nazwa) values ('{0}')",req.body.nazwa)).insertId;
+    res.redirect(format('/dodajPytanie/{0}',id));
 }
+
+var dodajPytanie = function(req, res){
+    var model = {
+        idTestu:req.params.id
+    }
+    ejs.renderFile("Views\\dodajPytanie.ejs",model,function(err,str){if(err) throw err; res.send(str);})
+}
+
+var zapiszPytanie = function(req, res){
+
+    con.query(format("insert into pytania (id_testu, tresc) values ({0},'{1}')",req.body.idTestu, req.body.tresc));
+    res.redirect(format('/dodajPytanie/{0}',req.body.idTestu));
+}
+
 
 app.get('/', stronaGlowna);
 app.get('/test/:id', (req, res) => res.send('tu bedą wyniki testu nr' + req.params.id));
-app.get('/edytuj/:id',edytujTest);
+app.get('/dodajPytanie/:id',dodajPytanie);
+app.get('/dodajTest',dodajTest);
 app.post('/zapisz', urlencodedParser,zapiszTest);
+app.post('/zapiszPytanie', urlencodedParser,zapiszPytanie);
+
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
