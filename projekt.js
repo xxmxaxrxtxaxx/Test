54730083c8c6a3bac6dab706e5c2fa6f881a45b1
xxmxaxrxtxaxx <xxmxaxrxtxaxx@gmail.com>
2018-05-06 15:00:55 +0200

    Poprawa logowania

--- projekt.js
+++ projekt.js
@@ -1,38 +1,50 @@
 const express = require('express');
-var mysql = require('sync-mysql');
-var bodyParser = require('body-parser')
-var ejs = require('ejs');
-var format = require('string-format');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+const mysql = require('sync-mysql');
+const bodyParser = require('body-parser')
+const ejs = require('ejs');
+const format = require('string-format');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const app = express();
 app.use(express.static(__dirname + '/public'));
 
-var con = new mysql({
+const con = new mysql({
     host: "localhost",
     user: "testyUser",
     password: "MocneHaslo",
     database: "bazaTestow"
 });
 
+const sess = {
+    secret: "tajny_klucz",
+    resave: false,
+    saveUninitialized: true,
+};
+
+
 passport.use(new LocalStrategy({
     usernameField: 'nazwa',
     passwordField: 'haslo',
-    // passReqToCallback: true,
-    session: false
+    session: true
 },
     function (username, password, done) {
-
-        //sparawdzić czy uzytkownik i hasło jest prawidłowe
-
-        return done(null, username);
+        var uzytkownicy = con.query(format("SELECT nazwa, haslo, czy_wykladowca, numer_indeksu, kierunek FROM uzytkownicy WHERE nazwa = '{0}'", username));
+        if (uzytkownicy.length > 0) {
+            if (uzytkownicy[0].haslo == password) {
+                return done(null, uzytkownicy[0]);
+            }
+        }
+        return done(null, false);
     }
 ));
-app.use(passport.initialize());
 app.use(urlencodedParser);
+app.use(session(sess));
+app.use(passport.initialize());
+app.use(passport.session());
 
 passport.serializeUser(function (user, done) {
     done(null, user);
@@ -47,7 +59,8 @@ var stronaGlowna = function (req, res) {
     var testy = con.query("select nazwa, id from testy");
     var model = {
         tytul: 'Strona główna',
-        testy: testy
+        testy: testy,
+        uzytkownik: req.user.nazwa
     }
     ejs.renderFile("Views\\home.ejs", model, function (err, str) { if (err) throw err; res.send(str); })
 }
@@ -74,8 +87,8 @@ var zapiszPytanie = function (req, res) {
     res.redirect(format('/dodajPytanie/{0}', req.body.idTestu));
 }
 
-var usunTest = function(req, res){
-    con.query(format("delete from testy where id={0}",req.params.idTestu));
+var usunTest = function (req, res) {
+    con.query(format("delete from testy where id={0}", req.params.idTestu));
     res.redirect(format('/'));
 }
 
@@ -86,19 +99,34 @@ var logowanie = function (req, res) {
     ejs.renderFile("Views\\logowanie.ejs", model, function (err, str) { if (err) throw err; res.send(str); })
 }
 
-app.get('/', stronaGlowna);
-app.get('/test/:id', (req, res) => res.send('tu bedą wyniki testu nr' + req.params.id));
-app.get('/dodajPytanie/:id', dodajPytanie);
-app.get('/dodajTest', dodajTest);
-app.post('/zapisz', urlencodedParser, zapiszTest);
-app.post('/zapiszPytanie', urlencodedParser, zapiszPytanie);
-app.get('/logowanie', logowanie)
-app.get('/usunTest/:idTestu',usunTest);
-app.post('/login',
+var czyZalogowany = (req, res, next) => {
+    //na potrzeby testów można na sztywno ustawić użytkownika, żeby nie trzeba było się cały czas logować. 
+    //Trzeba tylko od komentować linię  poniżej
+    //req.user= {nazwa:'zenek',haslo:'test',czy_wykladowca:0,numer_indeksu:1234,kierunek:'przyra'}
 
+    if (!req.isAuthenticated()) {
+        return res.redirect('/logowanie');
+    }
+    return next();
+};
+
+var wyloguj = function (req, res) {
+    req.logout();
+    res.redirect('/logowanie');
+};
+
+app.get('/', czyZalogowany, stronaGlowna);
+app.get('/test/:id', (req, res) => res.send('tu bedą wyniki testu nr' + req.params.id));
+app.get('/dodajPytanie/:id', czyZalogowany, dodajPytanie);
+app.get('/dodajTest', czyZalogowany, dodajTest);
+app.post('/zapisz', czyZalogowany, zapiszTest);
+app.post('/zapiszPytanie', czyZalogowany, zapiszPytanie);
+app.get('/logowanie', logowanie)
+app.get('/wyloguj', czyZalogowany, wyloguj);
+app.get('/usunTest/:idTestu', czyZalogowany, usunTest);
+app.post('/login',
     passport.authenticate('local', { failureRedirect: '/logowanie' }),
     function (req, res) {
-        console.log('login');
         res.redirect('/');
     });
 
